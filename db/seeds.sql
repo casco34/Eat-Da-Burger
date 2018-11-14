ec92eac9be993c538927783df978449009826117
erick casco <erick9c@gmail.com>
2018-11-14 16:15:08 -0600

    adding files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Fried burger Fried');
+INSERT INTO burgers (burger_name) VALUES ('Not a Burger');
+INSERT INTO burgers (burger_name) VALUES ('Awesome burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Giant burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('tiny Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie burger', false);
